47ef1030af5da5cc8788f7f7b467d499de66d91c
Chaitanyassr <chaitanyas0101@gmail.com>
2020-04-09 01:24:13 +0530

    removed logo

--- pages/index.vue
+++ pages/index.vue
@@ -1,39 +1,13 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        Tune_Share
-      </h1>
-      <h2 class="subtitle">
-        An app to share music
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
+ 
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
 
+import ToolBar from '~/components/Toolbar.vue';
 export default {
   components: {
-    Logo
+  ToolBar
   }
 }
 </script>
